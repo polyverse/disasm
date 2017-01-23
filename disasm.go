3dddcec5b5e27df6f1b1427d30dab8fbd64186ae
rsundahl <roy@polyverse.io>
2017-01-23 08:38:59 -0800

    go fmt

--- disasm.go
+++ disasm.go
@@ -4,14 +4,19 @@ package disasm
 import "C"
 
 type Ptr uintptr
-type InfoType C.struct_DisAsmInfo;
+type InfoType C.struct_DisAsmInfo
 
 func InfoInit(infoPtr *InfoType, start Ptr, end Ptr) {
-	C.DisAsmInfoInit(infoPtr, start, end);
-	return;
+	C.DisAsmInfoInit(infoPtr, start, end)
+	return
 } // InfoInit()
 
 func PrintGadget(infoPtr *InfoType, pc Ptr, doPrint bool) int {
-	var b C.int; if doPrint { b = 1; } else { b = 0; } 
-	return int(C.DisAsmPrintGadget(infoPtr, pc, b));
+	var b C.int
+	if doPrint {
+		b = 1
+	} else {
+		b = 0
+	}
+	return int(C.DisAsmPrintGadget(infoPtr, pc, b))
 } // PrintGadget()
--- disasm_test.go
+++ disasm_test.go
@@ -5,28 +5,28 @@ import "fmt"
 
 func TestDisAsm(t *testing.T) {
 	// Two dummy functions that "bracket" disasm.c (for demonstration purposes)
-        start := Ptr(0x40029bb); // Clearly, these two values only worked once.
-        end   := Ptr(0x4002a09); // Fix these, go get a real test.
+	start := Ptr(0x40029bb) // Clearly, these two values only worked once.
+	end := Ptr(0x4002a09)   // Fix these, go get a real test.
 
 	var info InfoType
-	InfoInit(&info, start, end);
+	InfoInit(&info, start, end)
 
-	gadgets := 0;
+	gadgets := 0
 	for pc := start; pc < end; pc = pc + 1 {
-		instructions := PrintGadget(&info, pc, false);
+		instructions := PrintGadget(&info, pc, false)
 
-		if (instructions > 0) {
-			fmt.Printf("GADGET AT: 0x%x (Length: %d)\n", pc, instructions);
-			PrintGadget(&info, pc, true);
-			fmt.Printf("\n");
-			gadgets++;
+		if instructions > 0 {
+			fmt.Printf("GADGET AT: 0x%x (Length: %d)\n", pc, instructions)
+			PrintGadget(&info, pc, true)
+			fmt.Printf("\n")
+			gadgets++
 		} // if
-	} // for 
+	} // for
 
-	fmt.Printf("GADGET COUNT BETWEEN 0x%x and 0x%x: %d (%d%%)\n", start, end, gadgets, gadgets * 100 / int((uintptr(end) - uintptr(start))));
+	fmt.Printf("GADGET COUNT BETWEEN 0x%x and 0x%x: %d (%d%%)\n", start, end, gadgets, gadgets*100/int((uintptr(end)-uintptr(start))))
 
 	// Fix me. start and end need to be set up with a dummy buffer that has predictable content
 	if gadgets == 0 {
-		t.Error("Failing, because start and end are very likely invalid addresses.");
+		t.Error("Failing, because start and end are very likely invalid addresses.")
 	} // if
 } // TestDisAsm()
